ca5fd67ce4c88f67464b3c48541f3b36b28ae085
Sharad Kumar <124381391+sharadnkp@users.noreply.github.com>
2026-09-05 23:53:03 +0530

    Time: 311 ms (63.49%), Space: 31.7 MB (75.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,21 +1,32 @@
 class Solution:
+    def isPossible(self, bloomDay, day, m, k):
+        count = 0
+        bouquets = 0
+
+        for bloom in bloomDay:
+            if bloom <= day:
+                count += 1
+                if count == k:
+                    bouquets += 1
+                    count = 0
+            else:
+                count = 0
+            
+        return bouquets >= m
+
+
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        if len(bloomDay) < m*k:
+            return -1
+
         low = min(bloomDay)
         high = max(bloomDay)
         ans = -1
+
         while low<=high:
             guess = (low+high)//2
-            adjacent = 0
-            bouquets = 0
-            for i in range(len(bloomDay)):
-                if bloomDay[i]<=guess:
-                    adjacent += 1
-                    if adjacent == k:
-                        bouquets += 1
-                        adjacent = 0
-                else:
-                    adjacent = 0
-            if bouquets>=m:
+            
+            if self.isPossible(bloomDay, guess, m, k):
                 ans = guess
                 high = guess-1
             else:
