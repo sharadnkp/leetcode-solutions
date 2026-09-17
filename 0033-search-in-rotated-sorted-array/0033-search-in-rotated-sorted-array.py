7f29262ec1738cbcad1834599533c862b368510b
Sharad Kumar <124381391+sharadnkp@users.noreply.github.com>
2026-09-17 23:41:24 +0530

    Time: 3 ms (3.94%), Space: 19.6 MB (10.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,34 +1,35 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums: list[int], target: int) -> int:
         low = 0
-        high = len(nums)-1
-        ans = 0
-        while low<=high:
-            mid = (low+high)//2
-            if nums[mid]>nums[len(nums)-1]:
-                low = mid+1
-            else:
-                ans = mid
-                high = mid-1
-        low = 0
-        high = ans-1
-        while low<=high:
-            mid = (low+high)//2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid]>target:
-                high = mid-1
-            else:
-                low = mid+1
+        high = len(nums) - 1
+        last = nums[-1]
 
-        low = ans
-        high = len(nums)-1
-        while low<=high:
-            mid = (low+high)//2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid]>target:
-                high = mid-1
+        while low <= high:
+            guess = (low + high) // 2
+
+            if nums[guess] == target:
+                return guess
+
+            # target and nums[guess] are in the right sorted part
+            elif target > last and nums[guess] > last:
+                if target < nums[guess]:
+                    high = guess - 1
+                else:
+                    low = guess + 1
+
+            # target is in the left sorted part
+            elif target <= last and nums[guess] <= last:
+                if target < nums[guess]:
+                    high = guess - 1
+                else:
+                    low = guess + 1
+
+            # target is in left part, mid is in right part
+            elif target > last and nums[guess] <= last:
+                high = guess - 1
+
+            # target is in right part, mid is in left part
             else:
-                low = mid+1
+                low = guess + 1
+
         return -1
