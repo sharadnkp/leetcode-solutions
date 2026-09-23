563391d40e0cf819089bfeb426cdd193d3f97e57
Sharad Kumar <124381391+sharadnkp@users.noreply.github.com>
2026-09-23 10:44:45 +0530

    Time: 47 ms (99.59%), Space: 31.1 MB (54.43%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,17 +1,26 @@
 class Solution:
-    def minOperations(self, A: List[int], x: int) -> int:
-        k = sum(A) - x
-        if k < 0: return -1 
-        best = -1
+    def minOperations(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+        # prefixes = [0]
+        # for num in nums:
+        #     prefixes.append(num + prefixes[-1])
         
-        s = i = 0
-        
-        for j, num in enumerate(A):
-            s += num
-            while s > k:
-                s -= A[i]
-                i += 1  
-            if s == k:
-                best = max(best, j - i + 1)
+        # target = prefixes[-1] - x
+        target = sum(nums) - x
+        if target == 0:
+            return n
+        elif target < 0:
+            return -1
 
-        return -1 if best < 0 else len(A) - best
+        res = -1
+        currsum = 0
+        l = 0
+        for r in range(n):
+            currsum += nums[r]
+            while currsum > target:
+                currsum -= nums[l]
+                l += 1
+            
+            if currsum == target:
+                res = max(res, (r - l + 1))    
+        return n - res if res != -1 else -1
